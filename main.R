bb5557ed3d2bb39d324054832a205b60cb50ecf9
mhale <mhale027@yahoo.com>
2016-12-30 16:35:32 -0500

    added

--- main.R
+++ main.R
@@ -5,8 +5,19 @@ library(xgboost)
 library(caret)
 library(dplyr)
 library(jpeg)
+library(foreach)
+library(doParallel)
+cl<-makeCluster(8)
+registerDoParallel(cl)
+
 
 setwd("~/projects/kaggle/FishId")
+folder <- 'input/train'
+print(list.files(folder))
+tmp <- lapply(list.files(folder), function(x) list.files(paste0(folder, '/', x)))
+files <- data.frame(label = rep(list.files(folder), sapply(tmp, length)), image = unlist(tmp))
+fish.files <- filter(files, label != "NoF")
+
 folder <- 'input/test_stg1'
 tests <- list.files(folder)
 
@@ -100,10 +111,10 @@ marks <- data.frame(image = fish.files$image,
 
 for (j in 1:nrow(fish.files)) {
       df <- filter(markers, image == fish.files$image[j])
-      marks$head_x[j] <- min(df[1,c(2,4)]) - 50
-      marks$tail_x[j] <- max(df[1,c(2,4)]) + 50
-      marks$head_y[j] <- min(df[1,c(3,5)]) - 50
-      marks$tail_y[j] <- max(df[1,c(3,5)]) + 50
+      marks$head_x[j] <- as.integer(df$head_x[1])
+      marks$tail_x[j] <- as.integer(df$tail_x[1])
+      marks$head_y[j] <- as.integer(df$head_y[1])
+      marks$tail_y[j] <- as.integer(df$tail_y[1])
 }
 save(marks, file = "marks.RData")
 
@@ -122,27 +133,98 @@ dims$cols <- as.numeric(dims$cols)
 save(dims, file = "dims.RData")
 
 box <- data.frame(image = marks$image, 
-                  head_x = marks$head_x * 300 / dims$rows,
-                  tail_x = marks$tail_x * 300 / dims$rows,
-                  head_y = marks$head_y * 150 / dims$cols,
-                  tail_y = marks$tail_y * 150 / dims$cols
+                  head_x = as.numeric(round(marks$head_x * 300 / dims$rows)),
+                  tail_x = as.numeric(round(marks$tail_x * 300 / dims$rows)),
+                  head_y = as.numeric(round(marks$head_y * 150 / dims$cols)),
+                  tail_y = as.numeric(round(marks$tail_y * 150 / dims$cols))
 )
 save(box, file = "box.300x150.RData")
 
 allmarked <- alltrain[-grep("NoF", files$label),]
+allmarked <- cbind(marks, allmarked)
 
-inTrain <- createDataPartition(1:nrow(box), p=.9, list = FALSE)
-inValid <- (1:nrow(box))[-inTrain]
 
-xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = box$head_x[inTrain])
-xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = box$head_x[inValid])
+
+
+
+
+load("allmarked.RData")
+inTrain <- createDataPartition(1:nrow(allmarked), p=.7, list = FALSE)
+inValid <- (1:nrow(allmarked))[-inTrain]
+
+
+
+h2o.init(nthreads = 8)
+h2o.removeAll()
+
+
+train.hex1 <- as.h2o(allmarked[inTrain[1:1200],], "train.hex1")
+train.hex2 <- as.h2o(allmarked[inTrain[1201:2320],], "train.hex2")
+train.hex3 <- as.h2o(allmarked[inTrain[1001:1500],], "train.hex3")
+train.hex4 <- as.h2o(allmarked[inTrain[1501:2000],], "train.hex4")
+train.hex5 <- as.h2o(allmarked[inTrain[2001:2320],], "train.hex5")
+train.hex <- rbind(train.hex1, train.hex2, train.hex3, train.hex4, train.hex5)
+# train.hex <- as.h20(allmarked[inTrain,], "train.hex")
+valid.hex <- as.h2o(allmarked[inValid,], "valid.hex")
+
+
+t1.hex <- h2o.importFile("h2o.frames", "t1.hex", header = TRUE)
+t2.hex <- h2o.importFile("h2o.frame.tr2", "t2.hex", header = TRUE)
+t3.hex <- h2o.importFile("h2o.frame.tr3", "t3.hex", header = TRUE)
+t4.hex <- h2o.importFile("h2o.frame.tr.4", "t4.hex", header = TRUE)
+t5.hex <- h2o.importFile("h2o.frame.tr.5", "t5.hex", header = TRUE)
+
+features <- 6:45005
+
+response <- 2
+
+system.time(dnn.1<-h2o.deeplearning(x = features,
+                        y =response,
+                        training_frame=train.hex,
+                        validation_frame=valid.hex,
+                        epochs=1, 
+                        stopping_rounds=5,
+                        overwrite_with_best_model=T,
+                        activation="Rectifier",
+                        # distribution="huber",
+                        hidden=c(120, 120)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = marks$head_x[inTrain])
+gc()
+xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = marks$head_x[inValid])
+gc()
+
+
+
+xg_eval_mae <- function (yhat, dtrain) {
+      y = getinfo(dtrain, "label")
+      err= mae(y, yhat)
+      return (list(metric = "error", value = err))
+}
+
 
 xgb.params.head_x <- list(
       eta = .1,
       objective = "reg:linear",
-      max_depth = 15,
-      colsample_bytree = .3,
-      subsample = .25
+      max_depth = 20,
+      colsample_bytree = .7,
+      subsample = .25,
+      base_score = 500
+      # feval = xg_eval_mae
 )
 
 
@@ -150,14 +232,15 @@ xgb.head_x <- xgb.train(data = xtrain,
                         watchlist = list(train = xtrain, valid = xvalid),
                         nrounds = 500,
                         early_stopping_rounds = 10,
-                        params = xgb.params.head_x
+                        params = xgb.params.head_x,
+                        maximize = FALSE
 )
 
 gc()
 
-xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = box$tail_x[inTrain])
+xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = as.integer(marks$tail_x[inTrain]))
 gc()
-xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = box$tail_x[inValid])
+xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = as.integer(marks$tail_x[inValid]))
 gc()
 xgb.params.tail_x <- list(
       eta = .1,
@@ -176,9 +259,9 @@ xgb.tail_x <- xgb.train(data = xtrain,
 )
 gc()
 
-xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = box$head_y[inTrain])
+xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = marks$head_y[inTrain])
 gc()
-xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = box$head_y[inValid])
+xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = marks$head_y[inValid])
 gc()
 xgb.params.head_y <- list(
       eta = .1,
@@ -198,9 +281,9 @@ xgb.head_y <- xgb.train(data = xtrain,
 
 gc()
 
-xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = box$tail_y[inTrain])
+xtrain <- xgb.DMatrix(data.matrix(allmarked[inTrain,]), label = marks$tail_y[inTrain])
 gc()
-xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = box$tail_y[inValid])
+xvalid <- xgb.DMatrix(data.matrix(allmarked[inValid,]), label = marks$tail_y[inValid])
 gc()
 xgb.params.tail_y <- list(
       eta = .1,
@@ -289,20 +372,57 @@ train.image <- function(id) {
       
 }
 
-means <- list()
-species <- list.files("input/train")
+patches <- list()
+species <- list.files("input/train")[-grep("NoF", list.files("input/train"))]
+count <- 0
 for (i in 1:length(species)) {
       print(i)
-      for (j in 1:length(grep(species[i], list.files("alldata")))) {
-            print(j)
-            if (i == 1) {
-                  temp.i <- load.image(list.files("alldata")[i])
+      for (j in 1:length(list.files(paste0("input/train/", species[i])))) {
+            count <- count + 1
+            print(count)
+            
+            temp.j <- load.image(paste0("input/train/", species[i], "/",
+                                        list.files(paste0("input/train/", species[i]))[j]))
+            temp.j <- imsub(temp.j, x > min(marks[count,c(2,3)]) & x < max(marks[count,c(2,3)]),
+                                    y > min(marks[count,c(4,5)]) & y < max(marks[count,c(4,5)]))
+            temp.j <- resize(temp.j, 201, 201, 1, 1)
+            if (j == 1) {
+                  sum.i <- temp.j
             } else {
-                  
+                  sum.i <- sum.i + temp.j
             }
+      patches[[i]] <- sum.i / length(list.files(paste0("input/train/", species[i])))
       }
 }
+save(patches, file = "patches.201x201.RData")
 
+for (i in 1:7) {
+      patch.i <- patches[[i]]
+      if (i == 1) {
+            mean.i <- patch.i
+      } else {
+            mean.i <- mean.i + patch.i
+      }
+}
+mean.patch <- mean.i / 7
 
-
-
+patch_size <- 75
+patch.edge <- (patch_size*2) + 1
+patch.i <- imrotate(resize(mean.patch, patch.edge, patch.edge), 0)
+img.i <- load.image(paste0("input/train/", files$label[10], "/", files$image[10]))
+img.i <- resize(img.i, dim(img.i)[1], dim(img.i)[2], 1, 1)
+search_size <- 25
+x1 <- search_size + patch_size + 1
+x2 <- dims$rows[1] - search_size - patch_size -1
+y1 <- search_size + patch_size +1
+y2 <- dims$cols[1] - search_size - patch_size -1
+params <- expand.grid(x = seq(x1, x2, search_size), y = seq(y1, y2, search_size))
+r  <- foreach(j = 1:nrow(params), .combine=rbind) %dopar% {
+      x     <- params$x[j]
+      y     <- params$y[j]
+      p     <- img.i[(x-patch_size):(x+patch_size), (y-patch_size):(y+patch_size),1,1]
+      score <- cor(as.vector(p), as.vector(patch.i))
+      score <- ifelse(is.na(score), 0, score)
+      data.frame(x, y, score)}
+r <- arrange(r, desc(score))
+head(r)
