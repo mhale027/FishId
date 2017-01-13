63ed789c551e8df558c9eadac0565aa1ef9f5aec
Matt Hale <mhale027@yahoo.com>
2017-01-13 16:36:17 -0500

    added

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import matplotlib.image as mpimg
 import cv2
 
 
-os.chdir("projects/kaggle/FishId")
+os.chdir("/home/matt/projects/kaggle/FishId")
 
 folder = "input/train"
 species = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
@@ -28,33 +28,29 @@ def list_files():
             fil = fil.append(pd.DataFrame(paths), ignore_index = True)
     return(fil)   
 
+files = list_files()
 fish_files = files[files['label'] != "NoF"]
-
 markers = pd.DataFrame.from_csv("markers.csv")
 
-img = cv2.imread("input/train/"+str(files['label'][1])+str(files['image'][1]))
+for i in range(1):
+    m = markers[i:i+1]
+    file = files[files['image'] == m.index[0]].iloc[i]['label']
+    num = markers.index[i]
+    img = cv2.imread("input/train/"+str(file)+'/'+str(num),1)
+    buffer = 20
+    x1 = int(min(m.iloc[0][0], m.iloc[0][2])) - buffer
+    y1 = int(min(m.iloc[0][1], m.iloc[0][3])) - buffer
+    x2 = int(max(m.iloc[0][0], m.iloc[0][2])) + buffer
+    y2 = int(max(m.iloc[0][1], m.iloc[0][3])) + buffer
 
 
+    cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 0), 2)
 
 
+print(img)
+
+cv2.imshow('img', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 
-def load_train():
-    X_train = []
-    X_train_id = []
-    y_train = []
-    start_time = time.time()
-
-    print('Read train images')
-    folders = ['ALB', 'BET', 'DOL', 'LAG', 'NoF', 'OTHER', 'SHARK', 'YFT']
-    for fld in folders:
-        index = folders.index(fld)
-        print('Load folder {} (Index: {})'.format(fld, index))
-        path = os.path.join('input', 'train', fld, '*.jpg')
-        files = glob.glob(path)
-        for fl in files:
-            flbase = os.path.basename(fl)
-            img = get_im_cv2(fl)
-            X_train.append(img)
-            X_train_id.append(flbase)
-            y_train.append(index)
